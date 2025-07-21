fea17425dec5660174b96f1b34b75ade8d287f30
SupermancyGreyrat <dwikyandicasaputra@gmail.com>
2025-07-21 14:48:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
      const btnContinue = document.getElementById('continueBtn');
     btnContinue.addEventListener('click', () => {
       // Redirect to new page on click
-      window.location.href = 'C:\Users\Lab TKJ\Documents\danil\rrr\index.html';
+      window.location.href = 'index2.html';
     });
