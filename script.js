64c00d0f7b0a118f68fe2c5e4c5338da8e067ef5
SupermancyGreyrat <dwikyandicasaputra@gmail.com>
2025-07-21 14:48:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+     const btnContinue = document.getElementById('continueBtn');
+    btnContinue.addEventListener('click', () => {
+      // Redirect to new page on click
+      window.location.href = 'C:\Users\Lab TKJ\Documents\danil\rrr\index.html';
+    });
